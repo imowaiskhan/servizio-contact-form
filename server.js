b8c3eb91aa33addffee762b303bf526ab9fe914b
imowaiskhan <owaiskhan3199@gmail.com>
2026-04-02 23:37:37 +0500

    solving timeout error

--- server.js
+++ server.js
@@ -58,6 +58,9 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   family: 4,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
@@ -65,7 +68,6 @@ const transporter = nodemailer.createTransport({
 });
 
 await transporter.verify();
-console.log("SMTP verified successfully");
 
     await transporter.sendMail({
       from: `"Servizio Primo Website" <${process.env.SMTP_FROM}>`,
